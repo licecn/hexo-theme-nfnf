464f3e79e6b827763f4b66737948a4da3876a644
lice <690612139@qq.com>
2020-02-22 16:38:18 +0800

    first commit

---
+++ scripts/process.js
@@ -0,0 +1,7 @@
+
+hexo.on('ready', function () {
+  hexo.log.info("=========================================");
+  hexo.log.info("  Welcome to use NFNF theme for hexo  ");
+  hexo.log.info("              你真是太帅了！           ");
+  hexo.log.info("=========================================");
+});
